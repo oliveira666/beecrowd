c886715031ae57f7e87ef7c15ab41c0348a68fa2
oliveira666 <ihagooliveira@hotmail.com>
2025-10-12 23:40:38 -0300

    1022

--- 1022/Program.cs
+++ 1022/Program.cs
@@ -12,55 +12,57 @@ class URI
         }
         while (N < 1 || N > Math.Pow(10, 4));
 
-        int[,] num = new int[N, 4];
+        int[,] numerador = new int[N, 4];
+        int[,] denominador = new int [N, 4];
         char[] c = new char[N];
         int[] pos = new int[2];
 
+        // Armazena todas variáveis nesse loop
         for (int i = 0; i < N; i++)
         {
             string[] input = Console.ReadLine().Split(' ');
-            num[i, 0] = int.Parse(input[0]); // N1
-            num[i, 1] = int.Parse(input[2]); // D1
+            numerador[i, 0] = int.Parse(input[0]); // N1
+            denominador[i, 0] = int.Parse(input[2]); // D1
             c[i] = char.Parse(input[3]);     // SINAL
-            num[i, 2] = int.Parse(input[4]); // N2
-            num[i, 3] = int.Parse(input[6]); // D2
+            numerador[i, 1] = int.Parse(input[4]); // N2
+            denominador[i, 1] = int.Parse(input[6]); // D2
         }
-
+        // Cálcula e mostra na tela
         for (int j = 0; j < N; j++)
         {
             switch (c[j])
             {
                 case '/': // (N1*D2)/(N2*D1)
-                    pos[0] = num[j, 0] * num[j, 3];
-                    pos[1] = num[j, 2] * num[j, 1];
+                    numerador[0,3] = numerador[j, 0] * denominador[j, 1];
+                    denominador[0,3] = numerador[j, 1] * denominador[j, 0];
 
-                    Console.Write(pos[0] + "/" + pos[1]);
-                    MDC(ref pos[0], ref pos[1]);
-                    Console.WriteLine(" = " + pos[0] + "/" + pos[1]);
+                    Console.Write(numerador[0,3] + "/" + denominador[0,3]);
+                    MDC(ref numerador[0,3], ref denominador[0,3]);
+                    Console.WriteLine(" = " + numerador[0,3] + "/" + denominador[0,3]);
                     break;
                 case '*': // (N1*N2) / (D1*D2)
-                    pos[0] = num[j, 0] * num[j, 2];
-                    pos[1] = num[j, 1] * num[j, 3];
+                    numerador[0,3] = numerador[j, 0] * numerador[j, 1];
+                    denominador[0,3] = denominador[j, 0] * denominador[j, 1];
 
-                    Console.Write(pos[0] + "/" + pos[1]);
-                    MDC(ref pos[0], ref pos[1]);
-                    Console.WriteLine(" = " + pos[0] + "/" + pos[1]);
+                    Console.Write(numerador[0,3] + "/" + denominador[0,3]);
+                    MDC(ref numerador[0,3], ref denominador[0,3]);
+                    Console.WriteLine(" = " + numerador[0,3] + "/" + denominador[0,3]);
                     break;
                 case '+': // (N1*D2 + N2*D1) / (D1*D2)
-                    pos[0] = (num[j, 0] * num[j, 3]) + (num[j, 2] * num[j, 1]);
-                    pos[1] = num[j, 1] * num[j, 3];
+                    numerador[0,3] = (numerador[j, 0] * denominador[j, 1]) + (numerador[j, 1] * denominador[j, 0]);
+                    denominador[0,3] = denominador[j, 0] * denominador[j, 1];
 
-                    Console.Write(pos[0] + "/" + pos[1]);
-                    MDC(ref pos[0], ref pos[1]);
-                    Console.WriteLine(" = " + pos[0] + "/" + pos[1]);
+                    Console.Write(numerador[0,3] + "/" + denominador[0,3]);
+                    MDC(ref numerador[0,3], ref denominador[0,3]);
+                    Console.WriteLine(" = " + numerador[0,3] + "/" + denominador[0,3]);
                     break;
                 case '-': // (N1*D2 - N2*D1) / (D1*D2)
-                    pos[0] = num[j, 0] * num[j, 3] - num[j, 2] * num[j, 1];
-                    pos[1] = num[j, 1] * num[j, 3];
+                    numerador[0,3] = numerador[j, 0] * denominador[j, 1] - numerador[j, 1] * denominador[j, 0];
+                    denominador[0,3] = denominador[j, 0] * denominador[j, 1];
 
-                    Console.Write(pos[0] + "/" + pos[1]);
-                    MDC(ref pos[0], ref pos[1]);
-                    Console.WriteLine(" = " + pos[0] + "/" + pos[1]);
+                    Console.Write(numerador[0,3] + "/" + denominador[0,3]);
+                    MDC(ref numerador[0,3], ref denominador[0,3]);
+                    Console.WriteLine(" = " + numerador[0,3] + "/" + denominador[0,3]);
                     break;
             }
         }
@@ -85,7 +87,7 @@ class URI
         numerador /= mdc;
         denominador /= mdc;
 
-        // Mantém o sinal no numerador
+        // Mantém o sinal no numeradorerador
         if (denominador < 0)
         {
             numerador *= -1;
